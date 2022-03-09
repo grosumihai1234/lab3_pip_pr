4fdbd82d382bcd58b55c0c47b5d3705f2b3f5fc6
Andrei Brailescu <andrei.brailescu@student.tuiasi.ro>
2022-03-09 13:30:57 +0200

    Am modificat putin frame ul sa arate a calculator

--- src/testgit/frame.java
+++ src/testgit/frame.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class frame extends JFrame {
 
@@ -41,17 +43,29 @@ public class frame extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(109, 315, 89, 23);
+		JButton btnNewButton = new JButton("+");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(534, 167, 41, 39);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(467, 315, 89, 23);
+		JButton btnNewButton_1 = new JButton("-");
+		btnNewButton_1.setBounds(534, 217, 41, 39);
 		contentPane.add(btnNewButton_1);
 		
 		textField = new JTextField();
-		textField.setBounds(131, 100, 412, 131);
+		textField.setBounds(163, 25, 412, 131);
 		contentPane.add(textField);
 		textField.setColumns(10);
+		
+		JButton btnX = new JButton("x");
+		btnX.setBounds(534, 267, 41, 39);
+		contentPane.add(btnX);
+		
+		JButton button = new JButton("=");
+		button.setBounds(534, 317, 41, 39);
+		contentPane.add(button);
 	}
 }
